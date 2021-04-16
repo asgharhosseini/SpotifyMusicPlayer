c1ef744a7b80789e731d1ef5fed9a78d8a202f30
asghar hosseini <asgharhosseini.v@gmail.com>
2021-04-16 12:23:12 +0430

    create data class for song

---
+++ app/src/main/java/ir/vbile/app/spotifymusicplayer/data/model/Song.kt
@@ -0,0 +1,9 @@
+package ir.vbile.app.spotifymusicplayer.data.model
+
+data class Song(
+    val mediaId: String = "",
+    val title: String = "",
+    val subtitle: String = "",
+    val songUrl: String = "",
+    val imageUrl: String = ""
+)
