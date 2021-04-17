b6476aa128d12c224442d5fc9e880593027a5b26
asghar hosseini <asgharhosseini.v@gmail.com>
2021-04-17 12:30:28 +0430

    getting songs form database and save in songs list

--- app/src/main/java/ir/vbile/app/spotifymusicplayer/service/FirebaseMusicSource.kt
+++ app/src/main/java/ir/vbile/app/spotifymusicplayer/service/FirebaseMusicSource.kt
@@ -1,10 +1,72 @@
 package ir.vbile.app.spotifymusicplayer.service
 
-class FirebaseMusicSource {
+
+import android.support.v4.media.MediaBrowserCompat
+import android.support.v4.media.MediaBrowserCompat.MediaItem.FLAG_PLAYABLE
+import android.support.v4.media.MediaDescriptionCompat
+import android.support.v4.media.MediaMetadataCompat
+import android.support.v4.media.MediaMetadataCompat.*
+import androidx.core.net.toUri
+import com.google.android.exoplayer2.source.ConcatenatingMediaSource
+import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import ir.vbile.app.spotifymusicplayer.data.remote.MusicDatabase
+import ir.vbile.app.spotifymusicplayer.service.State.STATE_CREATED
+import ir.vbile.app.spotifymusicplayer.service.State.STATE_INITIALIZING
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+class FirebaseMusicSource @Inject constructor(
+    private val musicDatabase: MusicDatabase
+) {
+
+    var songs = emptyList<MediaMetadataCompat>()
+
+    suspend fun fetchMediaData() = withContext(Dispatchers.IO) {
+        state = STATE_INITIALIZING
+        val allSongs = musicDatabase.getAllSongs()
+        songs = allSongs.map { song ->
+            MediaMetadataCompat.Builder()
+                .putString(METADATA_KEY_ARTIST, song.subtitle)
+                .putString(METADATA_KEY_MEDIA_ID, song.mediaId)
+                .putString(METADATA_KEY_TITLE, song.title)
+                .putString(METADATA_KEY_DISPLAY_TITLE, song.title)
+                .putString(METADATA_KEY_DISPLAY_ICON_URI, song.imageUrl)
+                .putString(METADATA_KEY_MEDIA_URI, song.songUrl)
+                .putString(METADATA_KEY_ALBUM_ART_URI, song.imageUrl)
+                .putString(METADATA_KEY_DISPLAY_SUBTITLE, song.subtitle)
+                .putString(METADATA_KEY_DISPLAY_DESCRIPTION, song.subtitle)
+                .build()
+        }
+        state = State.STATE_INITIALIZED
+    }
+
+    fun asMediaSource(dataSourceFactory: DefaultDataSourceFactory): ConcatenatingMediaSource {
+        val concatenatingMediaSource = ConcatenatingMediaSource()
+        songs.forEach { song ->
+            val mediaSource = ProgressiveMediaSource.Factory(dataSourceFactory)
+                .createMediaSource(song.getString(METADATA_KEY_MEDIA_URI).toUri())
+            concatenatingMediaSource.addMediaSource(mediaSource)
+        }
+        return concatenatingMediaSource
+    }
+
+    fun asMediaItems() = songs.map { song ->
+        val desc = MediaDescriptionCompat.Builder()
+            .setMediaUri(song.getString(METADATA_KEY_MEDIA_URI).toUri())
+            .setTitle(song.description.title)
+            .setSubtitle(song.description.subtitle)
+            .setMediaId(song.description.mediaId)
+            .setIconUri(song.description.iconUri)
+            .build()
+        MediaBrowserCompat.MediaItem(desc, FLAG_PLAYABLE)
+    }
 
     private val onReadyListeners = mutableListOf<(Boolean) -> Unit>()
 
-    private var state = State.STATE_CREATED
+    private var state: State = STATE_CREATED
         set(value) {
             if (value == State.STATE_INITIALIZED || value == State.STATE_ERROR) {
                 synchronized(onReadyListeners) {
@@ -14,26 +76,24 @@ class FirebaseMusicSource {
                     }
                 }
             } else {
-                field == value
+                field = value
             }
         }
 
     fun whenReady(action: (Boolean) -> Unit): Boolean {
-        if (state == State.STATE_CREATED || state == State.STATE_INITIALIZING) {
+        if (state == STATE_CREATED || state == STATE_INITIALIZING) {
             onReadyListeners += action
             return false
         } else {
             action(state == State.STATE_INITIALIZED)
             return true
         }
-
     }
+}
 
-
-    enum class State {
-        STATE_CREATED,
-        STATE_INITIALIZING,
-        STATE_INITIALIZED,
-        STATE_ERROR
-    }
+enum class State {
+    STATE_CREATED,
+    STATE_INITIALIZING,
+    STATE_INITIALIZED,
+    STATE_ERROR
 }
