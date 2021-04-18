6a7cc5c18aa67231b7f81b36a7ced9b5b01937bd
asghar hosseini <asgharhosseini.v@gmail.com>
2021-04-18 12:33:40 +0430

    change adapter setting for extend Base Adapter

--- app/src/main/java/ir/vbile/app/spotifymusicplayer/ui/adapter/SongAdapter.kt
+++ app/src/main/java/ir/vbile/app/spotifymusicplayer/ui/adapter/SongAdapter.kt
@@ -1,48 +1,15 @@
 package ir.vbile.app.spotifymusicplayer.ui.adapter
 
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.recyclerview.widget.AsyncListDiffer
-import androidx.recyclerview.widget.DiffUtil
-import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.RequestManager
 import ir.vbile.app.spotifymusicplayer.R
-import ir.vbile.app.spotifymusicplayer.data.model.Song
 import kotlinx.android.synthetic.main.list_item.view.*
 import javax.inject.Inject
 
 class SongAdapter @Inject constructor(
     private val glide: RequestManager
-) : RecyclerView.Adapter<SongAdapter.SongViewHolder>() {
-
-    class SongViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
-
-    private val diffCallback = object : DiffUtil.ItemCallback<Song>() {
-        override fun areItemsTheSame(oldItem: Song, newItem: Song): Boolean {
-            return oldItem.mediaId == newItem.mediaId
-        }
-
-        override fun areContentsTheSame(oldItem: Song, newItem: Song): Boolean {
-            return oldItem.hashCode() == newItem.hashCode()
-        }
-    }
-
-    private val differ = AsyncListDiffer(this, diffCallback)
-
-    var songs: List<Song>
-        get() = differ.currentList
-        set(value) = differ.submitList(value)
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SongViewHolder {
-        return SongViewHolder(
-            LayoutInflater.from(parent.context).inflate(
-                R.layout.list_item,
-                parent,
-                false
-            )
-        )
-    }
+) : BaseSongAdapter(R.layout.list_item) {
+    override val differ = AsyncListDiffer(this, diffCallback)
 
     override fun onBindViewHolder(holder: SongViewHolder, position: Int) {
         val song = songs[position]
@@ -50,7 +17,6 @@ class SongAdapter @Inject constructor(
             tvPrimary.text = song.title
             tvSecondary.text = song.subtitle
             glide.load(song.imageUrl).into(ivItemImage)
-
             setOnClickListener {
                 onItemClickListener?.let { click ->
                     click(song)
@@ -59,13 +25,5 @@ class SongAdapter @Inject constructor(
         }
     }
 
-    private var onItemClickListener: ((Song) -> Unit)? = null
 
-    fun setOnItemClickListener(listener: (Song) -> Unit) {
-        onItemClickListener = listener
-    }
-
-    override fun getItemCount(): Int {
-        return songs.size
-    }
 }
